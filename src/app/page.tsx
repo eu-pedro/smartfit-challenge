7808251332d0ede2a433912413e185dbf0482424
pedro henrique <pedro.henrike2812@gmail.com>
2024-02-05 16:30:06 -0300

    feat: criando legenda de informações da academia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,92 @@
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <>
+      <header className="p-5 w-full flex items-center justify-center bg-black">
+        <Image
+          src="/icones/logo.svg"
+          alt="Smartfit Logo"
+          width={120}
+          height={80}
+        />
+      </header>
+
+      <div className="p-5">
+        <h1 className="text-dark-gray mt-10 text-left uppercase">
+          Reabertura da Smart Fit
+        </h1>
+
+        <div className="w-[100px] h-3 bg-dark-gray mt-5" />
+
+        <p className="mt-5 text-justify">
+          O horário de funcionamento das nossas unidades está seguindo os
+          decretos de cada município. Por isso, confira aqui se a sua unidade
+          está aberta e as medidas de segurança que estamos seguindo.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+      </div>
+
+      <div className="p-5">
+        <div className="border-2 border-solid border-gray-500 p-5">
+          <div className="flex items-center gap-5">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/icones/icon-hour.png"
+              alt="Ìcone de Relógio"
+              width={50}
+              height={50}
             />
-          </a>
+            <p className="text-light-grey">Horário</p>
+          </div>
+
+          <div className="mt-5  border-b border-b-light-grey border-opacity-50">
+            <h3 className="text-3xl w-[65%] text-light-grey opacity-95 font-normal pl-4 pb-4">
+              Qual período quer treinar?
+            </h3>
+          </div>
+
+          <div className="flex justify-between py-4 border-b border-solid border-b-light-grey border-opacity-50">
+            <div className="flex gap-3">
+              <input type="checkbox" />
+              <p className="text-light-grey">Manhã</p>
+            </div>
+            <p className="text-light-grey">06:00 às 12:00</p>
+          </div>
+
+          <div className="flex justify-between py-4 border-b border-solid border-b-light-grey border-opacity-50">
+            <div className="flex gap-3">
+              <input type="checkbox" />
+              <p className="text-light-grey">Tarde</p>
+            </div>
+            <p className="text-light-grey">12:01 às 18:00</p>
+          </div>
+
+          <div className="flex justify-between py-4 border-b border-solid border-b-light-grey border-opacity-50">
+            <div className="flex gap-3">
+              <input type="checkbox" />
+              <p className="text-light-grey">Noite</p>
+            </div>
+            <p className="text-light-grey">18:01 às 23:00</p>
+          </div>
+
+          <div className="flex gap-4 mt-5">
+            <input type="checkbox" />
+            <p>Exibir unidades fechadas</p>
+          </div>
+
+          <p className="text-center mt-5">
+            Resultados encontrados: <span className="font-bold">0</span>
+          </p>
+
+          <div className="flex flex-col p-5 gap-5">
+            <button className="uppercase bg-yellow p-4 font-bold text-xl rounded-md">
+              Encontrar Unidade
+            </button>
+            <button className="uppercase outline outline-2 outline-light-grey text-xl p-4 rounded-md font-bold">
+              Limpar
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </>
+  )
 }
